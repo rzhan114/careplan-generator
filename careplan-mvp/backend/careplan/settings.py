2f3e559d0b4baeecc93f9266e106331d32a7f35e
Runtan Zhang <rzhan114@students.cs.ubc.ca>
2026-03-20 18:18:16 -0700

    change setting to fit the AWS

--- careplan-mvp/backend/careplan/settings.py
+++ careplan-mvp/backend/careplan/settings.py
@@ -28,11 +28,11 @@ ROOT_URLCONF = "careplan.urls"
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'careplan',
-        'USER': 'postgres',
-        'PASSWORD': 'postgres',
+        'NAME': os.environ.get('DB_NAME', 'careplan'),
+        'USER': os.environ.get('DB_USER', 'postgres'),
+        'PASSWORD': os.environ.get('DB_PASSWORD', 'postgres'),
         'HOST': os.environ.get('DB_HOST', 'db'),
-        'PORT': '5432',
+        'PORT': os.environ.get('DB_PORT', '5432'),
     }
 }
 
